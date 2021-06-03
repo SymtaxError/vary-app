461c27d9e1960bf3db301676d2cc5260cec10280
Aleksandr Tagilov <saniatagilov@gmail.com>
2021-06-04 00:03:51 +0300

    edit rules

--- Vary/app/src/main/java/com/example/vary/Database/AppDatabase.java
+++ Vary/app/src/main/java/com/example/vary/Database/AppDatabase.java
@@ -10,6 +10,16 @@ import com.example.vary.Models.CardModel;
 import com.example.vary.Models.CategoryModel;
 
 @Database(entities = {CardModel.class, CategoryModel.class}, version = 1)
+
+/*
+thJavac
+/Users/sntgl/StudioProjects/Symtax-Error-Android/Vary/app/src/main/java/com/example/vary/Database/AppDatabase.java:13: warning: Schema export directory is not provided to the annotation processor so we cannot export the schema. You can either provide `room.schemaLocation` annotation processor argument OR set exportSchema to false.
+public abstract class AppDatabase extends RoomDatabase {
+                ^
+Note: Some input files use or override a deprecated API.
+Note: Recompile with -Xlint:deprecation for details.
+1 warning
+ */
 public abstract class AppDatabase extends RoomDatabase {
 
     private static AppDatabase instance;
